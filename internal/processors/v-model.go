9f492eb4847c0ba548ba2e5cde658d25366f2aed
Sebastian Seggewiss <s.seggewiss@shopware.com>
2023-09-27 08:05:07 +0200

    1.0.3

--- internal/processors/v-model.go
+++ internal/processors/v-model.go
@@ -10,7 +10,7 @@ func NewModelProcessor() Processor {
 
 func (p *vModelProcessor) Process(options *ProcessorOptions) string {
 	// https://regex101.com/r/Q3VTcx/1
-	modelRegex := regexp.MustCompile(`(<` + options.ComponentName + `\n( *)[a-zA-Z0-9.\-+*#:_$(')@!=>"{%}\[\]? \n|/` + "`" + `&]*)v-model="(.*)"`)
+	modelRegex := regexp.MustCompile(`(<` + options.ComponentName + `\n( *)[a-zA-Z0-9.\-+*#:,_$(')@!=>"{%}\[\]? \n|/` + "`" + `&]*)v-model="(.*)"`)
 	modelFixed := modelRegex.ReplaceAllString(options.Text, "${1}{% if VUE3 %}\n${2}v-model:"+options.ModelName+"=\"$3\"\n$2{% else %}\n${2}v-model=\"${3}\"\n${2}{% endif %}")
 
 	return modelFixed
